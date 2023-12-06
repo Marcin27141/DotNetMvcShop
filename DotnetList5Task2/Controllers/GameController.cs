ac04f744000c0ed5f5a514606007d00c8aceb672
Marcin27141 <marcin.tkocz@op.pl>
2023-12-06 19:48:29 +0100

    added guessing game MVC

---
+++ DotnetList5Task2/Controllers/GameController.cs
@@ -0,0 +1,50 @@
+﻿using DotnetList5Task2.Models;
+using DotnetList5Task2.Models.Game;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetList5Task2.Controllers
+{
+    public class GameController : Controller
+    {
+        private const string INDEX_MESSAGE = "MESSAGE";
+        private static Random _random = new Random();
+        private static int _guessingCeiling = 10;
+        private static int secretNumber = 0;
+        private static int _guessesTaken = 0;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Set,{ceiling}")]
+        public IActionResult SetCeiling(int ceiling)
+        {
+            _guessingCeiling = ceiling;
+            TempData[INDEX_MESSAGE] = "New guessing range was set";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Route("Draw")]
+        public IActionResult DrawRandom()
+        {
+            secretNumber = _random.Next(0, _guessingCeiling);
+            _guessesTaken = 0;
+            TempData[INDEX_MESSAGE] = "New secret number was generated";
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Route("Guess,{guess}")]
+        public IActionResult GuessNumber(int guess)
+        {
+            _guessesTaken++;
+            var model = new GameViewModel
+            {
+                GuessesTaken = _guessesTaken,
+                SuccessfullyGuessed = guess == secretNumber,
+                MoreOrLess = Math.Sign(guess - secretNumber)
+            };
+            return View(model);
+        }
+    }
+}
---
+++ DotnetList5Task2/Models/Game/GameViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetList5Task2.Models.Game
+{
+    public class GameViewModel
+    {
+        public int GuessesTaken { get; set; }
+        public bool SuccessfullyGuessed { get; set; }
+        public int MoreOrLess { get; set; }
+    }
+}
