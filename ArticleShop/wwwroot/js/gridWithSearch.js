d9383b55d937cd3157fc1d8407a86a10751c8a76
Marcin27141 <marcin.tkocz@op.pl>
2023-12-15 13:28:24 +0100

    added bootstrap cards view

---
+++ ArticleShop/wwwroot/js/gridWithSearch.js
@@ -0,0 +1,16 @@
+﻿let input = document.getElementById("searchbar");
+let grid = document.getElementById("grid-col");
+let cards = document.getElementsByClassName("card");
+input.addEventListener("input", function () {
+    let filter = input.value.toLowerCase();
+
+    for (let i = 0; i < cards.length; i++) {
+        let card = cards[i];
+        let cardData = card.textContent || card.innerText;
+
+        if (cardData.toLowerCase().indexOf(filter) < 0)
+            card.classList.add('d-none');
+        else
+            card.classList.remove('d-none');
+    }
+});
