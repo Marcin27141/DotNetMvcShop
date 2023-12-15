23b0d78adad59ac3311570aaaab89f5b30cbf5c1
Marcin27141 <marcin.tkocz@op.pl>
2023-12-15 12:49:38 +0100

    added shop mvc with interactive filtering

---
+++ ArticleShop/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using ArticleShop.Models;
+using ArticleShop.Models.Database;
+using ArticleShop.Repositories.ArticleRepository;
+using ArticleShop.Repositories.CategoryRepository;
+using ArticleShop.Repositories.ImageRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ArticleShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly IArticleRepository _articleRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public ShopController(IArticleRepository articleRepository, ICategoryRepository categoryRepository)
+        {
+            _articleRepository = articleRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            var selectableCategories = categories.Select(c => new SelectListItem() { Text = c.Name, Value = c.Name.ToLower() }).OrderBy(c => c.Text);
+            var withAllOption = selectableCategories.Prepend(new SelectListItem() { Text = "All", Value = "all" });
+            return View(new ShopViewModel(
+                await _articleRepository.GetAllAsync(),
+                withAllOption
+                ));
+        }
+    }
+}
---
+++ ArticleShop/Models/ShopViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ArticleShop.Models.Database;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ArticleShop.Models
+{
+    public class ShopViewModel
+    {
+        public IEnumerable<Article> Articles { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public Guid? SelectedCategory { get; set; }
+
+        public ShopViewModel(IEnumerable<Article> articles, IEnumerable<SelectListItem> categories)
+        {
+            Articles = articles;
+            Categories = categories;
+        }
+    }
+}
